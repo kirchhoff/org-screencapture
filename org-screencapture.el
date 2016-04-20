1d2732468120115cb62d1c4a412a063e8554f39c
Wenda Tang <wdkirchhoff@163.com>
2016-04-20 18:24:28 +0800

    first commit

---
+++ org-screencapture.el
@@ -0,0 +1,52 @@
+;; Copyright (C) 2015/2016 wdkirchhoff 
+
+;; Author: wdkirchhoff <wdkirchhoff@163.com>
+;; Keywords: org
+
+;; This file is not part of GNU Emacs.
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation; either version 3, or (at your option)
+;; any later version.
+;;
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+;;
+;; You should have received a copy of the GNU General Public License
+;; along with GNU Emacs.  If not, see <http://www.gnu.org/licenses/>.
+
+(defun my-org-screenshot ()
+  "Take a screenshot into a time stamped unique-named file in the
+same directory as the org-buffer and insert a link to this file."
+  (interactive)
+  (org-display-inline-images)
+  (setq filename
+        (concat
+         (make-temp-name
+          (concat (file-name-nondirectory (buffer-file-name))
+                  "_imgs/"
+                  (format-time-string "%Y%m%d_%H%M%S_")) ) ".png"))
+  (unless (file-exists-p (file-name-directory filename))
+    (make-directory (file-name-directory filename)))
+					; take screenshot
+  (if (eq system-type 'darwin)
+      (progn
+	(call-process-shell-command "screencapture" nil nil nil nil " -s " (concat
+									    "\"" filename "\"" ))
+	(call-process-shell-command "convert" nil nil nil nil (concat "\"" filename "\" -resize  \"50%\"" ) (concat "\"" filename "\"" ))
+	))
+  (if (eq system-type 'gnu/linux)
+      (call-process "import" nil nil nil filename))
+					; insert into file if correctly taken
+  (if (file-exists-p filename)
+      (insert (concat "[[file:" filename "]]")))
+  (org-display-inline-images)
+  (message filename)
+  )
+
+(global-set-key (kbd "C-c s c") 'my-org-screenshot)
+
+(provide 'org-screencapture)
